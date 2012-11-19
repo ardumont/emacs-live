4a011054b5b7d30796aeae6270a3fe65e4da1bc5
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-11-19 21:29:10 +0100

    Added some binding i like from my old starter-kit use

--- packs/live/tony-pack/config/bindings.el
+++ packs/live/tony-pack/config/bindings.el
@@ -1,5 +1,14 @@
 ;; Place your bindings here.
 
-;; For example:
-;;(define-key global-map (kbd "C-+") 'text-scale-increase)
-;;(define-key global-map (kbd "C--") 'text-scale-decrease)
+(define-key global-map (kbd "C-+") 'text-scale-increase)
+(define-key global-map (kbd "C--") 'text-scale-decrease)
+
+;; make C-w to cut (even in paredit-mode)
+(global-set-key (kbd "C-w") 'kill-region)
+(define-key paredit-mode-map (kbd "C-w") 'kill-region)
+
+;; yank
+(global-set-key (kbd "C-y") 'yank)
+
+(global-set-key (kbd "C-v") (lambda () (interactive) (next-line 10)))
+(global-set-key (kbd "M-v") (lambda () (interactive) (previous-line 10)))
